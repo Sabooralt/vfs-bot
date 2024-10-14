3f37596f0ff43c032c4dfb86de692e9cae88b29d
Saboor <saboor2202e@aptechgdn.net>
2024-10-14 05:18:03 +0500

    chromium path

--- controllers.js
+++ controllers.js
@@ -1,9 +1,8 @@
 const { connect } = require("puppeteer-real-browser");
-const { execSync } = require('child_process');
 
 
 require("dotenv").config();
-const chromiumPath = execSync('which chromium').toString().trim();
+
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 let browser;
@@ -11,7 +10,7 @@ const newBrowser = async (user, url) => {
   try {
     const { browser, page } = await connect({
       headless: true,
-      executablePath: chromiumPath,
+      executablePath: '/usr/bin/chromium-browser',
 
       ignoreDefaultArgs: ['--disable-extensions'],
       args: [
