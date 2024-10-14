93649f0c8d7c5a2c71e1b0b5a6a2d566a6e168f0
Saboor <saboor2202e@aptechgdn.net>
2024-10-15 03:33:25 +0500

    added default timeout

--- controllers.js
+++ controllers.js
@@ -30,7 +30,6 @@ const newBrowser = async (user, url) => {
   try {
 
 
-    await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36");
     await page.setViewport({ width: 1920, height: 1080 });
 
     page.setDefaultNavigationTimeout(0)
