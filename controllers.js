168e86456b07982b713b8f161b5ace255df827e6
Saboor <saboor2202e@aptechgdn.net>
2024-10-15 03:29:28 +0500

    User agent for linux

--- controllers.js
+++ controllers.js
@@ -30,7 +30,7 @@ const newBrowser = async (user, url) => {
   try {
 
 
-    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.3");
+    await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36");
     await page.setViewport({ width: 1920, height: 1080 });
 
     page.setDefaultNavigationTimeout(0)
